b9c515f308868892d7d7d04d6469497822d92319
matej lesko <lesko.matej.pu@gmail.com>
2018-02-19 06:48:05 +0100

    NEW: simple example of interface

---
+++ interface/simple.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Node interface {
+	GetValue() int
+	SetValue(int)
+}
+
+type BasicNode struct {
+	value int
+}
+
+func (n *BasicNode) GetValue() int {
+	return n.value
+}
+
+func (n *BasicNode) SetValue(val int) {
+	n.value = val
+}
+
+func NewBasicNode() *BasicNode {
+	return new(BasicNode)
+}
+
+type PowerNode struct {
+	value int
+}
+
+func (n *PowerNode) GetValue() int {
+	return n.value
+}
+
+func (n *PowerNode) SetValue(val int) {
+	n.value = val * val
+}
+
+func NewPowerNode() *PowerNode {
+	return new(PowerNode)
+}
+
+func main() {
+	var n Node
+	n = NewBasicNode()
+	n.SetValue(10)
+	fmt.Println("Node value is:", n.GetValue())
+	n = NewPowerNode()
+	n.SetValue(10)
+	fmt.Println("Node value is:", n.GetValue())
+
+}
